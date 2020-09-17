33657f8986e0e8db4b06bd39f4e73db06fd82a94
fisher <ihqgfboy@126.com>
2020-09-17 18:16:14 +0800

    dcrutil/v1.4.0

---
+++ dcrutil/README.md
@@ -0,0 +1,26 @@
+dcrutil
+=======
+
+
+[![Build Status](http://img.shields.io/travis/hdfchain/hdfd.svg)](https://travis-ci.org/hdfchain/hdfd)
+[![ISC License](http://img.shields.io/badge/license-ISC-blue.svg)](http://copyfree.org)
+[![GoDoc](http://img.shields.io/badge/godoc-reference-blue.svg)](http://godoc.org/github.com/hdfchain/hdfd/dcrutil)
+
+Package dcrutil provides hdfchain-specific convenience functions and types.
+A comprehensive suite of tests is provided to ensure proper functionality.
+
+This package was developed for hdfd, a full-node implementation of Decred which
+is under active development by Company 0.  Although it was primarily written for
+hdfd, this package has intentionally been designed so it can be used as a
+standalone package for any projects needing the functionality provided.
+
+## Installation and Updating
+
+```bash
+$ go get -u github.com/hdfchain/hdfd/dcrutil
+```
+
+## License
+
+Package dcrutil is licensed under the [copyfree](http://copyfree.org) ISC
+License.
---
+++ dcrutil/amount.go
@@ -0,0 +1,46 @@
+// Copyright (c) 2013, 2014 The btcsuite developers
+// Copyright (c) 2015 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package dcrutil
+
+import v2 "github.com/hdfchain/hdfd/dcrutil/v2"
+
+// AmountUnit describes a method of converting an Amount to something
+// other than the base unit of a coin.  The value of the AmountUnit
+// is the exponent component of the decadic multiple to convert from
+// an amount in coins to an amount counted in atomic units.
+type AmountUnit = v2.AmountUnit
+
+// These constants define various units used when describing a coin
+// monetary amount.
+const (
+	AmountMegaCoin  = v2.AmountMegaCoin
+	AmountKiloCoin  = v2.AmountKiloCoin
+	AmountCoin      = v2.AmountCoin
+	AmountMilliCoin = v2.AmountMilliCoin
+	AmountMicroCoin = v2.AmountMicroCoin
+	AmountAtom      = v2.AmountAtom
+)
+
+// Amount represents the base coin monetary unit (colloquially referred
+// to as an `Atom').  A single Amount is equal to 1e-8 of a coin.
+type Amount = v2.Amount
+
+// NewAmount creates an Amount from a floating point value representing
+// some value in the currency.  NewAmount errors if f is NaN or +-Infinity,
+// but does not check that the amount is within the total amount of coins
+// producible as f may not refer to an amount at a single moment in time.
+//
+// NewAmount is for specifically for converting DCR to Atoms (atomic units).
+// For creating a new Amount with an int64 value which denotes a quantity of
+// Atoms, do a simple type conversion from type int64 to Amount.
+// See GoDoc for example: http://godoc.org/github.com/hdfchain/hdfd/dcrutil#example-Amount
+func NewAmount(f float64) (Amount, error) {
+	return v2.NewAmount(f)
+}
+
+// AmountSorter implements sort.Interface to allow a slice of Amounts to
+// be sorted.
+type AmountSorter = v2.AmountSorter
